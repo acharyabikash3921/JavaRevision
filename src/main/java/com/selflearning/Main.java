08d220d8a613f575fd6db43f797706080d2057aa
Bikash Acharya <acharyabikash3921@gmail.com>
2025-08-20 23:32:54 -0400

    Date Time features explaiation

--- src/main/java/com/selflearning/Main.java
+++ src/main/java/com/selflearning/Main.java
@@ -1,8 +1,12 @@
 package com.selflearning;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("First Program");
+        System.out.println(LocalDateTime.now());
 
     }
 }
