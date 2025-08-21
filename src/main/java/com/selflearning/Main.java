ef9cd6af267cd8aba1b0a2b0533b5cbbecdf488e
Bikash Acharya <acharyabikash3921@gmail.com>
2025-08-20 22:25:00 -0400

    First Push

--- src/main/java/com/selflearning/Main.java
+++ src/main/java/com/selflearning/Main.java
@@ -4,7 +4,7 @@ package com.selflearning;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("First Program");
 
     }
 }
