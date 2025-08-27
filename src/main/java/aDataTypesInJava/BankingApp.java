d686c7bc12c2df365ded86a7684737019dfcfa77
Bikash Acharya <acharyabikash3921@gmail.com>
2025-08-26 23:57:05 -0400

    Added Debugging file

--- src/main/java/aDataTypesInJava/BankingApp.java
+++ src/main/java/aDataTypesInJava/BankingApp.java
@@ -3,14 +3,11 @@ package aDataTypesInJava;
 public class BankingApp {
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount("Ac1", "Alice", 1000);
-
-
         account1.credit(1000);
-    account1.credit(2000);
-    account1.displayTransactionHistory();
-
-    account1.debit(2000);
-    account1.displayTransactionHistory();
+        account1.credit(2000);
+        account1.displayTransactionHistory();
+        account1.debit(2000);
+        account1.displayTransactionHistory();
         System.out.println(account1.balance);
     }
 }
